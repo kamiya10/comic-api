3e17899a58d070c19c0b7a6be584a367d5459ef0
Kamiya <kamiya@kamiya.app>
2024-11-19 10:27:22 +0800

    feat: search endpoint

---
+++ server/api/v1/mhgui/search.get.ts
@@ -0,0 +1,46 @@
+import { load } from 'cheerio';
+import type { CheerioAPI } from 'cheerio';
+
+export default defineEventHandler(async (event) => {
+  const { q } = getQuery<{ q?: string }>(event);
+
+  if (!q) {
+    throw createError({
+      status: 400,
+    });
+  }
+
+  const $ = await $fetch<CheerioAPI>(`https://tw.manhuagui.com/s/${encodeURI(q)}_o1.html`, {
+    parseResponse: load,
+  });
+
+  if (!$) throw createError({
+    status: 500,
+  });
+
+  const result = $('.book-result>ul>li')
+    .map((_, el) => {
+      const li = $(el);
+      const a = li.find('.bcover');
+
+      const url = a.attr('href');
+      const id = url?.split('/').at(-2);
+
+      if (!id) {
+        console.error('/v1/mhgui/search id is not defined.');
+        throw createError({
+          status: 502,
+        });
+      }
+
+      const status = li.find('.tt').text();
+      const title = li.find('dt > a').text();
+
+      return { id, title, status };
+    }).toArray();
+
+  setHeader(event, 'Content-Type', 'application/json; charset=utf-8');
+  setHeader(event, 'Access-Control-Allow-Origin', '*');
+
+  return result;
+});
